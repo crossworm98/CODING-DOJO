bbb3e831ca11031629f32034ee7499c5d78c286c
crossworm98 <kaydnburns98@gmail.com>
2022-02-09 14:26:26 -0500

    update

--- Python/Tutorial/functions_intermediate1.py
+++ Python/Tutorial/functions_intermediate1.py
@@ -1,8 +1,8 @@
 x = [ [5,2,3], [10,8,9] ] 
 x[1][0] = 15
 students = [
-     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-     {'first_name' : 'John', 'last_name' : 'Rosales'}
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
 ]
 
 students[0]['first_name'] = 'billy'
@@ -21,10 +21,10 @@ print(z)
 #Iterate Through a List of Dictionaries
 
 students = [
-         {'first_name':  'Michael', 'last_name' : 'Jordan'},
-         {'first_name' : 'John', 'last_name' : 'Rosales'},
-         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
 def iterateDictionary(students):
     for x in students:
@@ -40,10 +40,10 @@ first_name - KB, last_name - Tonel
 #Get Values From a List of Dictionaries
 
 students = [
-         {'first_name':  'Michael', 'last_name' : 'Jordan'},
-         {'first_name' : 'John', 'last_name' : 'Rosales'},
-         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
 def iterateDictionary2(first_name, students):
     for x in students:
@@ -54,8 +54,8 @@ iterateDictionary2('last_name', students)
 #Iterate Through a Dictionary with List Values
 
 dojo = {
-   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
 def printInfo(dojo):
     for k, v in dojo.items():
